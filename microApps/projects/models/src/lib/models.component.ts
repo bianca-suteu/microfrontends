efa28d51c80783bee9a5551e83287858a3bcdb46
Bianca Suteu <bianca.suteu@co-era.com>
2024-09-18 14:45:58 +0300

    generate model lib

---
+++ microApps/projects/models/src/lib/models.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-models',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      models works!
+    </p>
+  `,
+  styles: ``
+})
+export class ModelsComponent {
+
+}
