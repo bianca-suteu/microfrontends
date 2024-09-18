5947cf56a58ee972267b472223878303aecf882e
Bianca Suteu <bianca.suteu@co-era.com>
2024-09-18 13:18:03 +0300

    Link shell with mfe1

--- microApps/projects/shell/src/app/app.component.ts
+++ microApps/projects/shell/src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'shell';
+  name = new FormControl('');
+  message= '';
+
+  // constructor(private sharedInfoService: SharedInfoService) {
+  //   this.name.valueChanges.subscribe(val => {
+  //     console.log(val);
+  //     this.sharedInfoService.sendMessage(val || '');
+  //   })
+
+  //   this.sharedInfoService.message$.subscribe(message => {
+  //     this.message = message;
+  //     console.log("Shared", this.message)
+  //   });
+  // }
 }
--- microApps/projects/shell/src/app/app.routes.ts
+++ microApps/projects/shell/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: '',
+  component: DashboardComponent,
+  pathMatch: 'full',
+},
+{
+  path: 'mfe1',
+  loadComponent: () =>
+    loadRemoteModule('mfe1', './Component').then((m) => m.AppComponent),
+},
+];
